c6721804de348f9f426274561ccc39cf6311b778
Aniket Jaiswal <jaiswalaniket302@gmail.com>
2024-02-22 14:33:37 +0530

    pattern

--- Patterns/pattern8.cpp
+++ Patterns/pattern8.cpp
@@ -1,4 +1,36 @@
-//   1
-//  22
-// 333
+//    1
+//   22
+//  333
 // 4444
+
+#include <iostream>
+using namespace std;
+
+int main()
+{
+
+    int n;
+    cout << "Enter no. of rows : ";
+    cin >> n;
+
+    int row = 1;
+    while (row <= n)
+    {
+        int spaces = n - row;
+        while (spaces)
+        {
+            cout << " ";
+            spaces--;
+        }
+        int col = 1;
+        while (col <= row)
+        {
+            cout << row;
+            col++;
+        }
+        cout << endl;
+        row++;
+    }
+
+    return 0;
+}
