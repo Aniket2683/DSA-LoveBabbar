cdd6e8f94b640729c9e8f36044fa4d2d60b2b3a0
Aniket Jaiswal <jaiswalaniket302@gmail.com>
2024-02-22 14:54:06 +0530

    pattern

--- Patterns/pattern9.cpp
+++ Patterns/pattern9.cpp
@@ -2,3 +2,36 @@
 //  234
 //   34
 //    4
+
+#include <iostream>
+using namespace std;
+
+int main()
+{
+
+    int n;
+    cout << "Enter no of Rows :";
+    cin >> n;
+
+    int row = 1;
+    while (row <= n)
+    {
+        int sapces = row - 1;
+        while (sapces)
+        {
+            cout << " ";
+            sapces--;
+        }
+
+        int col = row;
+        while (col <= n)
+        {
+            cout << col;
+            col++;
+        }
+        cout << endl;
+        row++;
+    }
+
+    return 0;
+}
